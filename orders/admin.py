c43036670d8f082a553bfe0c18db11d9e304350f
Viktorio135 <102095652+Viktorio135@users.noreply.github.com>
2025-03-06 21:07:32 +0200

    1.1

--- orders/admin.py
+++ orders/admin.py
@@ -2,7 +2,22 @@ from django.contrib import admin
 
 from .models import Item, Order, Table, OrderItem
 
-admin.site.register(Item)
-admin.site.register(Table)
-admin.site.register(Order)
-admin.site.register(OrderItem)
+
+@admin.register(Item)
+class ItemAdmin(admin.ModelAdmin):
+    pass
+
+@admin.register(Table)
+class TableAdmin(admin.ModelAdmin):
+    pass
+
+
+@admin.register(Order)
+class OrderAdmin(admin.ModelAdmin):
+    pass
+
+
+@admin.register(OrderItem)
+class OrderItemAdmin(admin.ModelAdmin):
+    pass
+
